097bb00553460f5342dc8970d2dca6e812182a3d
Andriy K <asd.andriy.k@gmail.com>
2023-11-28 13:40:42 +0200

    Rename index.html -> index.php

---
+++ index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="utf-8">
+    <meta name="robots" content="noindex, nofollow">
+    <link rel="stylesheet" href="css/style_st.css">
+    <title>Дитячий плед 3 В 1</title>
+    <link rel="icon" href="images/10676.png" type="image/png">
+    <link rel="shortcut icon" href="images/10676.png" type="image/png">
+    
+    <!-- Метатеги та посилання на стилі -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=480">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/slick.css">
+	<link rel="stylesheet" href="css/style.css">
+
+    <!-- Підключення скриптів -->
+    <script src="js/jquery-2.2.4.min.js" type="text/javascript"></script>
+    <script type="text/javascript" async="" src="js/watch.js"></script>
+    <script type="text/javascript" src="js/validation.js"></script>
+    <script type="text/javascript" src="js/ab.min.js"></script>
+    <script type="text/javascript" src="js/slick.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+</head>
+<body>
+        <style>
+            .plug {
+                padding-top: 50px;
+                text-align: center;
+                font-size: 72px;
+                background-color: green;
+                color: #e43315;
+            }
+        </style>
+    
+    <!-- Основний контейнер сторінки -->
+    <div class="main_wrapper">
+        <!-- Верхній блок (шапка) -->
+        <header class="offer_section offer3">
+            <!-- Заголовок та інформація про товар -->
+            <h1 class="main_title">Дитячий плед</h1>
+            <div class="info_block">
+                <p class="subtitle">З капюшоном 3 в 1</p>
+                <div class="discount">
+                    <p><b>Знижка 23%</b></p>
+                </div>
+                <img src="images/product.png" alt="">
+            </div>
+
+            <!-- Ціновий блок -->
+            <div class="price_block">
+            
+                <!-- Стара ціна -->
+                <div class="price_item old">
+                    <div class="text">Стара ціна:</div>
+                    <div class="value">
+                        <x-oldprice>740</x-oldprice>
+                        <x-currency>грн</x-currency>
+                    </div>
+                </div>
+
+                <!-- Нова ціна -->
+                <div class="price_item new">
+                   <div class="text">Ціна по акції</div>
+                    <div class="value">
+                        <x-newprice>569</x-newprice>
+                        <x-currency>грн</x-currency>
+                    </div>
+                </div>
+                
+            </div>
+
+            <!-- Блок переваг товару -->
+            <div class="benefits_block clearfix">
+                <!-- Перевага 1 -->
+                <div class="benefit_item">
+                    <img src="images/benefit1.png" alt="">
+                    <p>ІГРАШКА + ПОДУШКА + ХУДІ</p>
+                </div>
+
+                <!-- Перевага 2 -->
+                <div class="benefit_item">
+                    <img src="images/benefit2.png" alt="">
+                    <p>УНІВЕРСАЛЬНИЙ РОЗМІР</p>
+                </div>
+
+                <!-- Перевага 3 -->
+                <div class="benefit_item">
+                    <img src="images/benefit3.png" alt="">
+                    <p>КОМФОРТ ДЛЯ ДИТИНИ</p>
+                </div>
+            </div>
+
+            <!-- Кнопка замовлення -->
+            <a href="#order_form" class="button">Замовити зі знижкою 23%</a>
+
+            <!-- Секція з описом -->
+            <section class="description_section">
+                <!-- Заголовок та інформація -->
+                <h2 class="title"><span>ГОЛОВНІ ОСОБЛИВОСТІ</span></h2>
+                <div class="top_info">
+                    <img class="image" src="images/feature1.jpg" alt="">
+                    <h4>Ваша дитина буде рада такому подарунку</h4>
+                </div>
+
+                <!-- Перший абзац опису -->
+                <p align="justify">
+                    Його особливість у тому, що розстебнувши блискавку на капюшоні, 
+                    Ви зможете трансформувати худі в плюшеву іграшку! Худі виготовлено з м'якого і приємного матеріалу. 
+                    На відміну від колючих светрів Ваша дитина не буде вередувати перед тим як одягнути його.
+                </p>
+
+                <!-- Зображення для першого абзацу -->
+                <img class="image" src="images/errka.jpeg" alt="">
+
+                <!-- Другий абзац опису -->
+                <p align="justify">
+                    Толстовка зігріє в холодні зимові вечори, а кумедна іграшка завжди супроводжуватиме вашу дитину! 
+                    Ви можете взяти іграшку з собою в поїздку, і вона відмінно впорається з функцією подушки.
+                </p>
+            </section>
+        </header>
+
+        <!-- Секція з характеристиками -->
+        <section class="characteristics_section">
+            <!-- Зображення та список характеристик -->
+            <img class="image" src="images/1231.jpeg" alt="">
+            <ul class="characteristics__list">
+                <li>Стать: хлопчик, дівчинка</li>
+                <li>Зріст: до 140 см</li>
+                <li>Сезон: демісезон</li>
+                <li>Матеріал: поліестер 70%, бавовна 30%</li>
+                <li>Подушка: 20 х 20 см</li>
+                <li>Рукав: з манжетою</li>
+            </ul>
+
+            <!-- Кнопка замовлення -->
+            <a href="#order_form" class="button">Замовити зі знижкою</a>
+        </section>
+
+        <!-- Секція з відеообзором -->
+        <section class="description_section">
+            <!-- Заголовок та контейнер відео -->
+            <h2 class="title"><span>Відео огляд</span> </h2>
+            <div class="video-container plug">
+                <!-- <video width="100%" autoplay="" loop="" muted="" controls="" playsinline="" src="media/video.mp4"></video> -->
+                <h1>[video]</h1>        
+            </div>
+        </section>
+
+        <!-- Секція з відгуками -->
+        <section class="characteristics_section">
+            <!-- Заголовок та список відгуків -->
+            <h2 class="title"><span>Відгуки</span> покупців</h2>
+            <div class="reviews_list4">
+                <div class="review_item">
+                    <div class="text_block">
+                        <img class="offer_image" src="images/rev.jpg" alt="">
+                        <div class="author_info">м. Львів, Ірина</div>
+                        <p>Я давно хотіла на кухню гарну тертку, але не могла вибрати. Вирішила ось у вас замовити, якість клас! Особливе дякую за швидку доставку)))</p>
+                    </div>
+                </div>
+                <div class="review_item">
+                    <div class="text_block">
+                        <img class="offer_image" src="images/rev.jpg" alt="">
+                        <div class="author_info">м. Київ, Світлана</div>
+                        <p>Ця овочерізка – це просто супер-знахідка! Зараз, коли вона в мене є, я навіть не знаю, як я жила без неї! Вона абсолютно незамінна у мене на кухні! Вона робить практично все. Нарізки - на будь-який смак, насадок дуже багато, плюс друшляк!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Секція з кроками замовлення -->
+        <section class="order_steps_info">
+            <!-- Заголовок та інформація про кроки замовлення -->
+            <h2 class="title"><span>Як зробити</span> замовлення?</h2>
+            <div class="order_steps2 dark clearfix">
+                <div class="step_item">
+                    <img src="images/lqbsn-c-.png" alt="">
+                    <div class="separator white"></div>
+                    <h4>Заявка</h4>
+                    <p>Залишаєте заявку на сайті</p>
+                </div>
+                <div class="step_item">
+                    <img src="images/cdkcvewk.png" alt="">
+                    <div class="separator white"></div>
+                    <h4>Дзвінок</h4>
+                    <p>Наш менеджер уточнює деталі замовлення</p>
+                </div>
+                <div class="step_item">
+                    <img src="images/1uhpt7k3.png" alt="">
+                    <div class="separator white"></div>
+                    <h4>Доставка</h4>
+                    <p>Швидко доставимо замовлення</p>
+                </div>
+                <div class="step_item">
+                    <img src="images/6rg2brss.png" alt="">
+                    <div class="separator white"></div>
+                    <h4>Отримання</h4>
+                    <p>Оплачуєте при отриманні готівкою або карткою</p>
+                </div>
+
+            </div>
+            <p style="text-align:center; padding:1em; font-weight: 700;color: white;background: #000000d1;">
+            Акція діє з
+            <script>
+                d = new Date();
+                p = new Date(d.getTime() - 5 * 86400000);
+                monthA = '01,02,03,04,05,06,07,08,09,10,11,12'.split(',');
+                var w = p.getDate();
+                document.write(p.getDate() + '.' + 
+                monthA[p.getMonth()] + '.' + p.getFullYear());
+            </script>
+                до 
+            <script>
+                d = new Date();
+                p = new Date(d.getTime() + 0 * 86400000);
+                monthA = '01,02,03,04,05,06,07,08,09,10,11,12'.split(',');
+                var w = p.getDate();
+                document.write(p.getDate() + '.' + 
+                monthA[p.getMonth()] + '.' + p.getFullYear());
+            </script>
+                включно по всій території України, окрім тимчасово окупованих територій.
+            </p>
+        </section>
+        
+        <header class="offer_section offer3">
+            <h1 class="main_title">Дитячий плед</h1>
+            <div class="info_block">
+                <p class="subtitle">З капюшоном 3 в 1</p>
+                <div class="discount">
+                    <p><b>Знижка 23%</b></p>
+                </div>
+                <img src="images/product.png" alt="">
+            </div>
+            <div class="price_block">
+                <div class="price_item old">
+                    <div class="text">Стара ціна:</div>
+                    <div class="value">
+                        <x-oldprice>740</x-oldprice>
+                        <x-currency>грн</x-currency>
+                    </div>
+                </div>
+                <div class="price_item new">
+                    <div class="text">Ціна по акції</div>
+                    <div class="value">
+                        <x-newprice>569</x-newprice>
+                        <x-currency>грн</x-currency>
+                    </div>
+                </div>
+            </div>
+            <div class="benefits_block clearfix">
+                <div class="benefit_item">
+                    <img src="images/benefit1.png" alt="">
+                    <p>ІГРАШКА + ПОДУШКА + ХУДІ</p>
+                </div>
+                <div class="benefit_item">
+                    <img src="images/benefit2.png" alt="">
+                    <p>УНІВЕРСАЛЬНИЙ РОЗМІР</p>
+                </div>
+                <div class="benefit_item">
+                    <img src="images/benefit3.png" alt="">
+                    <p>КОМФОРТ ДЛЯ ДИТИНИ</p>
+                </div>
+            </div>
+            <h2 class="title"><span>Залишайте заявку</span></h2>
+            <form id="order_form" class="main-order-form order_form orderformcdn" action="/demo/thank.php" method="post">
+                <input class="field" type="text" name="name" placeholder="Введіть Ваше ім'я" required="">
+                <input class="field" type="tel" name="phone" id="phone" placeholder="Введіть Ваш телефон" required="">
+                <button class="button">Оформити замовлення</button>
+            </form>
+        </header>
+
+        <!-- Нижня частина (футер) -->
+        <footer class="footer_section">
+            <!-- Інформація про авторські права та посилання -->
+            <center>
+                <div class="copyright">
+                    <p style="font-size: 16px; color: #000;">Copyright ©<script>document.write(new Date().getFullYear());</script></p>
+                    <p style="margin-bottom: 70px;"><a style="text-decoration: none; color: #000; font-size: 16px;" href="politics.html" target="_blank">Політика конфіденційності</a></p>
+                </div>
+            </center>
+        </footer>
+
+    </div>
+</body>
+</html>
